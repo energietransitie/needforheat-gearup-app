f9ceefedfd21eae0aceae524dbde78fcc9828269
NormalNickYT <nickkoster2001@hotmail.com>
2023-11-08 21:58:37 +0100

    fix: Enelogic is now available in the device list, and when Enelogic is connected, the user is automatically directed to the device screen.

--- apps/expo/src/screens/home/AddOnlineDataSourceScreen.tsx
+++ apps/expo/src/screens/home/AddOnlineDataSourceScreen.tsx
@@ -5,9 +5,10 @@ import ManualContent from "@/components/common/ManualContent";
 import StatusIndicator from "@/components/common/StatusIndicator";
 import Box from "@/components/elements/Box";
 import useDevice from "@/hooks/device/useDevice";
+import useDeviceActivate from "@/hooks/device/useDeviceActivate";
 import useTranslation from "@/hooks/translation/useTranslation";
 import { HomeStackParamList } from "@/types/navigation";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ExternalProviderListItem from "../settings/ExternalProviderScreen/_externalProviderListItem";
 import { setAuthState, MANUAL_URL, getAuthState } from "@/constants";
 import { useOpenExternalLink } from "@/hooks/useOpenExternalLink";
@@ -18,6 +19,9 @@ import { z } from "zod";
 import useActivateCloudFeed from "@/hooks/cloud-feed/useActivateCloudFeed";
 import useCloudFeeds from "@/hooks/cloud-feed/useCloudFeeds";
 import useDeepLinks from "@/hooks/useDeepLinks";
+import { crc16xmodem } from "crc";
+import useUser from "@/hooks/user/useUser";
+import { UserContext } from "@/providers/UserProvider";
 
 export type ExternalProviderItem = { name: string; icon: string; description: string; is_connected: boolean };
 
@@ -30,9 +34,56 @@ export default function AddOnlineDataSourceScreen({ navigation, route }: AddOnli
     const { openUrl } = useOpenExternalLink();
     const { data, isFetching } = useCloudFeeds();
     const { mutate: activate } = useActivateCloudFeed();
+    const deviceActivateMutation = useDeviceActivate();
+    const { user } = useContext(UserContext);
 
     const onCancel = () => {
         navigation.goBack();
+    }
+
+    const createDevice = async (cloudFeedName: string) => {
+        const retries = 3; // retries because device name has a possibility to have the same name using generateRandomHex(6)
+        let deviceCreated = false;
+
+        try {
+            const buildingId = user?.buildings[0]?.id;
+            if (buildingId === undefined) {
+                console.error("buildingId is undefined");
+                return;
+            }
+
+            for (let attempt = 1; attempt <= retries; attempt++) {
+                const crc16XMODEM = crc16xmodem(cloudFeedName).toString(16).toUpperCase();
+
+                const createDevicePayload = {
+                    name: crc16XMODEM + '-' + generateRandomHex(6),
+                    activationSecret: generateRandomHex(32),
+                    buildingId: buildingId,
+                };
+
+                try {
+                    await deviceActivateMutation.mutateAsync(createDevicePayload);
+                    deviceCreated = true;
+                    break;
+                } catch (error) {
+                    console.error(`Device creation failed (attempt ${attempt}):`, error);
+                }
+            }
+            if (!deviceCreated) {
+                console.error(`Failed to create device after ${retries} attempts.`);
+            }
+        } catch (error) {
+            console.error("Device creation failed:", error);
+        }
+    };
+
+    function generateRandomHex(length: number) {
+        const characters = "0123456789ABCDEF";
+        let result = "";
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * 16));
+        }
+        return result;
     }
 
     useDeepLinks(async link => {
@@ -65,6 +116,13 @@ export default function AddOnlineDataSourceScreen({ navigation, route }: AddOnli
                     : undefined,
                 preset: "done",
             });
+
+            navigation.navigate("HomeScreen");
+
+            if (providerName) {
+                await createDevice(providerName);
+            }
+
         } catch (error) {
             const errorMsg = `${t("common.unknown_error")}${(error as Error)?.message ? `\n\n${(error as Error).message}` : ""
                 }`;
