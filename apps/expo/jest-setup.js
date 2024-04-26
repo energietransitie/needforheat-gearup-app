7f9172f6d94c4ff18d1c846f4bdd3f029eb3acff
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2024-04-26 16:58:24 +0200

    Fix: PR comment

--- apps/expo/jest-setup.js
+++ apps/expo/jest-setup.js
@@ -1,13 +1,5 @@
 // /* eslint-disable no-undef, import/no-extraneous-dependencies */
 
-// // Import Jest Native matchers
-// import "@testing-library/jest-native/extend-expect";
-
-// // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-// jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
-// // Setup Reanimated mocking for Drawer navigation
-// global.ReanimatedDataMock = { now: () => Date.now() };
-// // require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
 import { NativeModules as RNNativeModules } from "react-native";
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 jest.mock("@react-native-firebase/dynamic-links");
@@ -25,6 +17,7 @@ RNNativeModules.RNGestureHandlerModule = RNNativeModules.RNGestureHandlerModule 
   dropGestureHandler: jest.fn(),
   updateGestureHandler: jest.fn(),
 };
+
 RNNativeModules.PlatformConstants = RNNativeModules.PlatformConstants || {
   forceTouchAvailable: false,
 };
