9b6f5f085b6f5debd9a237377660a0963f0f7cd4
Harris <16213031+Labhatorian@users.noreply.github.com>
2024-06-02 13:18:09 +0200

    Script: Improved yarn scripts and preinstall script

--- apps/expo/scripts/eas-build-pre-install.sh
+++ apps/expo/scripts/eas-build-pre-install.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
+# Ensure ~/.ssh directory exists
 mkdir -p ~/.ssh
 
-# Real origin URL is lost during the packaging process, so if your
-# submodules are defined using relative urls in .gitmodules then
-# you need to restore it with:
-#
-# git remote set-url origin git@github.com:example/repo.git
+# Decode the base64 encoded SSH key and save it to ~/.ssh/id_ed25519
+echo "$GH_SSH_KEY" | base64 -d > ~/.ssh/id_ed25519
 
-# restore private key from env variable and generate public key
-echo "$GH_SSH_KEY" | base64 -d > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+# Set correct permissions for the private key
+chmod 600 ~/.ssh/id_ed25519
 
-# add your git provider to the list of known hosts
+# Generate public key from the private key
+ssh-keygen -y -f ~/.ssh/id_ed25519 > ~/.ssh/id_ed25519.pub
+
+# Add GitHub to the list of known hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
